8acad44107e2928b5d82c08f01ed1454ce731cfd
Rafek731 <raf.mazur.000@gmail.com>
2025-10-23 12:10:09 +0200

    Added .evaluate(0 finctionality to models

--- ML/Model.py
+++ ML/Model.py
@@ -1,12 +1,33 @@
 import numpy as np
+from .Evaluator import Evaluator
 
 class Model:
     """Parent class for all models"""
     def __init__(self):
         pass
-
-    def fit(X: np.ndarray, y: np.ndarray):
+    
+    # this method must be overwritten
+    def fit(X: np.ndarray, y: np.ndarray) -> None:
         raise NotImplemented
     
-    def predict(X: np.ndarray):
+    # this method must be overwritten
+    def predict(features: np.ndarray) -> int:
         raise NotImplemented
+    
+    def evaluate(self, X: np.ndarray, y: np.ndarray, intermediate_states: bool = False) -> None:
+        eval = Evaluator()
+        if intermediate_states:
+            eval.print_head()
+
+        for features, label in zip(X, y):
+            prediction = self.predict(features)
+            eval.judge(prediction, label)
+            if intermediate_states:
+                eval.print()
+
+        print('\nFinal:')
+        print(eval.metrics)
+        print()
+        print(eval.conf_matrix)
+ 
+
